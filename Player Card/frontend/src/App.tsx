ce35318cf64cd384750731155b03ddbecde0a250
vidhyutcables <vidhyutcables@gmail.com>
2025-12-03 14:16:35 +0530

    Delete Player Card directory

--- Player Card/frontend/src/App.tsx
+++
@@ -1,6 +0,0 @@
-
-import React from 'react';
-
-export default function App(){
-  return <div>Player Card Generator Frontend Placeholder</div>;
-}
--- Player Card/server/index.js
+++
@@ -1,8 +0,0 @@
-
-// Minimal Express + Sharp server (placeholder)
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const app = express();
-app.use(fileUpload());
-app.post('/generate', (req,res)=>{ res.send('OK'); });
-app.listen(3001, ()=>console.log('Server running'));
